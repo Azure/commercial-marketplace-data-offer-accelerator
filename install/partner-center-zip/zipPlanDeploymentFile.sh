dee1cf6ed29abd50ba6a890451df3162b0c9ef13
David Starr <david@elegantcode.com>
2020-08-03 18:49:36 -0700

    writeapp.zip to local directory

--- install/partner-center-zip/zipPlanDeploymentFile.sh
+++ install/partner-center-zip/zipPlanDeploymentFile.sh
@@ -1,4 +1,4 @@
 # a script to zip the deployment package for a plan
 # this script ensure no OS files are included from non-Windows systems
-rm ../partner-center-config/app.zip
-zip ../partner-center-config/app.zip createUiDefinition.json mainTemplate.json viewDefinition.json ./nestedtemplates/storageAccount.json
+rm ./app.zip
+zip ./app.zip createUiDefinition.json mainTemplate.json viewDefinition.json ./nestedtemplates/storageAccount.json
