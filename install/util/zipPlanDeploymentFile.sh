147f569970df67114101e085372c9632bb774b7b
David Starr <david@elegantcode.com>
2020-08-06 20:42:44 -0700

    deletes previous zip and doesn't include OS files.

--- install/util/zipPlanDeploymentFile.sh
+++ install/util/zipPlanDeploymentFile.sh
@@ -1,11 +1,13 @@
 # this script file creates the app.zip file to be uploaded for each Offer > Plan in Partner Center
-# the "app.zip" output file goes to the "partner-center-config" folder
+# the output file goes to the "partner-center-config/app.zip" folder
 # this script ensure no OS files are included from non-Windows systems
 
 zipFile='../partner-center-config/app.zip'
 
 if [ -f $zipFile ]; then
-    rm 4zipFile
+    rm $zipFile
 fi
 
-zip -m -r -n '.DS_Store' $zipFile ../partner-center-zip
+cd ../partner-center-zip
+
+zip -r -D -n '.DS_Store' $zipFile .
