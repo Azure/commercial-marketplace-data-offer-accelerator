7864c1fbee0c63926f525698e146f82e3d4a0957
ashisa <ashisa78@live.com>
2020-08-21 20:46:34 +0530

    merging

--- install/util/UpdateFunctionApp.ps1
+++ install/util/UpdateFunctionApp.ps1
@@ -38,10 +38,12 @@ else {
 if ($updateApp) {
   Write-Host "Updating function app..."
   if ( [environment]::OSVersion.Platform -eq "Unix") {
-    Start-Process -FilePath "pwsh" -Args "-Command Publish-AzWebapp -ResourceGroupName $resourceGroup -Name $functionAppName -ArchivePath (Resolve-Path $destinationPath).Path -Force"
+    $processInfo = Start-Process -FilePath "pwsh" -Args "-Command Publish-AzWebapp -ResourceGroupName $resourceGroup -Name $functionAppName -ArchivePath (Resolve-Path $destinationPath).Path -Force" -PassThru
+    $processInfo.WaitForExit()
   }
   else {
-    Start-Process -FilePath "powershell.exe" -Args "Publish-AzWebapp -ResourceGroupName $resourceGroup -Name $functionAppName -ArchivePath (Resolve-Path $destinationPath).Path -Force"
+    $processInfo = Start-Process -FilePath "powershell.exe" -Args "Publish-AzWebapp -ResourceGroupName $resourceGroup -Name $functionAppName -ArchivePath (Resolve-Path $destinationPath).Path -Force" -PassThru
+    $processInfo.WaitForExit()
   }
   Write-Host "Update complete."
 }
